11ad44805ff8091e9fb419d0dd8792d216b9789d
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-06-09 23:25:10 -0400

    IN-37 integrated login form with the landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,24 @@
 import React from 'react'
 import './App.css';
-import logo from './assets/logo.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
-import { Nav, Navbar } from 'react-bootstrap';
+import Login from './components/Login'
+import Landing from './components/Landing'
 
-function LandingContent() {
-  return (
-    <div className="content">
-      <h1>Helping Africa’s brightest minds go from idea to MVP to market </h1>
-      <br/>
-      <h3>An e-learning social platform tailored for instructors, partners, and young entrepreneurs.</h3>
-      <ul>
-        <li>Individual profile creates and company profile creations for entrepreneurs.</li>
-        <li>E-learning through prerecorded content that can be consumed in a self pace manner.</li>
-        <li>Community features to interact with partners and other entrepreneurs.</li>
-        <li>Deliverable completion/submission and event scheduling on a calendar.</li>
-      </ul>
-    </div>
-  )
-}
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar className="navbar-orange">
-        <Navbar.Brand href="#home" className="mr-auto">
-          <img 
-            alt="alt text"
-            src={logo}
-            width="100"
-            height="80"
-            />{' '}
-            Ampact
-        </Navbar.Brand>
-        <Nav className="d-flex align-items-end">
-          <Nav.Link>Login</Nav.Link>
-          <Nav.Link>Register</Nav.Link>
-        </Nav>
-      </Navbar>
-      <LandingContent/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing}/>
+        <Route path="/login" component={Login}/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Landing.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Link from "react-router-dom/Link";
+import logo from '../assets/logo.png'
+import { Nav, Navbar } from 'react-bootstrap'
+import './Landing.css'
+
+function LandingContent() {
+    return (
+      <div className="content">
+        <h1>Helping Africa’s brightest minds go from idea to MVP to market </h1>
+        <br/>
+        <h3>An e-learning social platform tailored for instructors, partners, and young entrepreneurs.</h3>
+        <ul>
+          <li>Individual profile creates and company profile creations for entrepreneurs.</li>
+          <li>E-learning through prerecorded content that can be consumed in a self pace manner.</li>
+          <li>Community features to interact with partners and other entrepreneurs.</li>
+          <li>Deliverable completion/submission and event scheduling on a calendar.</li>
+        </ul>
+      </div>
+    )
+  }
+
+function Landing() {
+return (
+    <div>
+      <Navbar className="navbar">
+        <Navbar.Brand href="https://www.africanimpact.ca/the-african-impact-challenge" className="mr-auto">
+          <img 
+              alt="alt text"
+              src={logo}
+              width="100"
+              height="80"
+              />{' '}
+              Ampact
+          </Navbar.Brand>
+          <Nav className="d-flex align-items-end">
+          <Nav.Link><Link to="/login">Login</Link></Nav.Link>
+          <Nav.Link>Register</Nav.Link>
+          </Nav>
+        </Navbar>
+      <LandingContent/>
+    </div>
+    )
+}
+
+export default Landing;
