1944e9163ce6af657e55d1e7779f3542a547f2ec
Kris <63805048+ravence@users.noreply.github.com>
2021-06-10 18:56:30 -0400

    Revert "Merge branch 'IN-20' of https://github.com/UTSCCSCC01/project-insert-name-here into IN-20"
    This reverts commit 5ec3ab12ba488165b7234d5d82506c455a2873ed, reversing
    changes made to 8df47020ec3c7637ebdb6da955d1678c35c2d52e.

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -3,87 +3,69 @@ const Group = require("../models/Group");
 const router = express.Router();
 const bodyParser = require('body-parser');
 
-// Get all groups 
-router.get("/", async (req, res) => {
-    let group
+// get all groups 
+router.get("/group", async (req, res) => {
     try {
-        group = await Group.find(); 
+        let group = await Group.find(); 
         console.log(group);
-        res.status(200).send(group); 
-    } catch(err) {                                  // Serverside error occured, thus return error message
-        // console.error(err); 
-        res.status(500).send({error: err.message});  
+
+        res.status(200); 
+        res.send(group); 
+    } catch(err) {
+        console.error(err); 
+        res.status(400); 
+        res.send({error: err}); 
     }
 });
 
-// Get specific group 
-router.get("/:groupID", getGroup, async (req, res) => {
-    res.send(res.group); 
-});
-
-// Update a group attribute(s) 
-router.patch("/:groupID", getGroup, async (req, res) => {
-    // If a groupName is provided in the body of the patch request, then update the group name for the group 'groupID'
-    if (req.body.name != null) {
-        res.group.name = req.body.name
-    }
-    // If a groupAbout is provided in the body of the patch request, then update the group about for the group 'groupID'
-    if (req.body.about != null) {
-        res.group.about = req.body.about
-    }
-    // If a groupPicture is provided in the body of the patch request, then update the profile picture for the group 'groupID'
-    if (req.body.picture != null) {
-        res.group.picture = req.body.picture
-    }
+// get specific group 
+router.get("/group/:groupID", async (req, res) => {
     try {
-        const updatedGroup = await res.group.save()
-        res.json(updatedGroup) // Successfully updatedGroup, thus return updated group
-    } catch(error) { //Post request had invalid arguments
-        res.status(400).json({ message: error.message })
-    }
-});
-
-// Create new group 
-router.post("/", async (req, res) => {
-   // try {
-        let name = req.body.name; 
-        let about = req.body.about;
-        let picture = req.body.picture;
-        let group = new Group({name: name, about: about, picture: picture}); 
-        
-
-       try {
-            const savedGroup = await group.save(); 
-            console.log("Group created"); 
-            res.status(201).json(savedGroup);            // Successfully created group, thus return new group info as json
-       } catch (err) {                                   // Error in creating a new group, thus return error message (bad data)
-            res.status(400).json({ message: err.message})
-       }
-}); 
-
-// Add new member to group 
-router.post("/:groupID", getGroup, async (req, res) => {
-    // TODO: check userID is valid 
-    let userID = req.body.userID; 
-
-    res.group.members.push(userID); 
-    res.group.save(); 
-    console.log("Group member added"); 
-
-    res.status(200).send(); 
-}); 
-
-// Middleware used to getGroup from MongoDB database
-async function getGroup(req, res, next) {
-    let group
-    try {
-        group = await Group.findById(req.params.groupID); //.populate('members'); 
+        let group = await Group.findById(req.params.groupID); //.populate('members'); 
+        res.send(group); 
     } catch(error) {
-       return res.status(404).json({ message: 'Group does not exist'})
+        console.error(error);
+        res.status(400); 
+        res.send({error: "Group does not exist"}); 
     }
-    res.group = group
-    next()
-}
+});
+
+// add group 
+router.post("/group", async (req, res) => {
+    try {
+        let groupName = req.body.groupName; 
+        let group = new Group({name: groupName}); 
+        group.save(); 
+        console.log("Group created"); 
+
+        res.status(200); 
+        res.send();
+    } catch(error) {
+        res.status(400); 
+        res.send({error: "Group creation failed"})
+    }
+}); 
+
+// add member to group 
+router.post("/group/:groupID", async (req, res) => {
+    try { 
+        // TODO: check userID is valid 
+        let userID = req.body.userID; 
+        let groupID = req.params.groupID; 
+
+        let group = await Group.findById(groupID);
+        group.members.push(userID); 
+        group.save(); 
+        console.log("Group member added"); 
+
+        res.status(200); 
+        res.send();
+    } catch(error) {
+        res.status(400); 
+        console.error(error);
+        res.send({error: "Group does not exist"})
+    }
+}); 
 
 // Remove member from group 
 router.delete("/:groupID/:userID", async (req, res) => {
