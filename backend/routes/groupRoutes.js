8fc1767beef54b4ee387c590c21b64ccb7f25e84
Chris <christophersuhse@gmail.com>
2021-06-11 19:12:35 -0400

    Added delete group feature.
    Added delete group feature.

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express"); 
+const { findByIdAndDelete } = require("../models/Group");
 const Group = require("../models/Group");
 const User = require("../models/User");
 const router = express.Router();
@@ -11,19 +12,19 @@ router.get("/", async (req, res) => {
         if (!groups) return res.json([]);
         return res.json(groups);
     });
-   /* let group
-    try {
-        group = await Group.find().populate('members', '_id');
-        console.log(group);
-        res.status(200).send(group); 
-    } catch(err) {                                  // Serverside error occured, thus return error message
-        // console.error(err); 
-        res.status(500).send({error: err.message});  
-    }*/
 });
 
-// Get specific group  getGroup,
+// Get specific group 
 router.get("/:groupID", async (req, res) => {
+    let groupId = req.params.groupID;
+    Group.findById(groupId, (err, groups) => {
+        if (err) return res.status(500).send({success: false, message: err.toString()});
+        console.log(groups);
+        if (!groups) return res.status(404).send({success: false, message: "Group not found"});
+        return res.json(groups);
+    }); 
+});
+/*router.get("/:groupID", async (req, res) => {
     let groupId = req.params.groupID;
     Group.findById(groupId, (err, groups) => {
         if (err) return res.status(500).send({success: false, message: err.toString()});
@@ -31,30 +32,35 @@ router.get("/:groupID", async (req, res) => {
         if (!groups) return res.status(401).send({success: false, message: "Bad input"});
         return res.json(groups);
     });
-    /*res.send(res.group); 
-    console.log(res.group)*/
-});
+    res.send(res.group); 
+    console.log(res.group)
+});*/
 
 // Update a group attribute(s) 
-router.patch("/:groupID", getGroup, async (req, res) => {
+router.patch("/:groupID", async (req, res) => {
+    let groupId = req.params.groupID;
+    let groupP
+    Group.findById(groupId, async (err, groups) => {
+        if (req.body.name != null) {
+            groups.name = req.body.name
+        }
+        // If a groupAbout is provided in the body of the patch request, then update the group about for the group 'groupID'
+        if (req.body.about != null) {
+            groups.about = req.body.about
+        }
+        // If a groupPicture is provided in the body of the patch request, then update the profile picture for the group 'groupID'
+        if (req.body.picture != null) {
+            groups.picture = req.body.picture
+        }
+        try {
+            const updatedGroup = await groups.save()
+            res.json(updatedGroup) // Successfully updatedGroup, thus return updated group
+        } catch(error) { //Post request had invalid arguments
+            res.status(400).json({ message: error.message })
+        }
+    });
     // If a groupName is provided in the body of the patch request, then update the group name for the group 'groupID'
-    if (req.body.name != null) {
-        res.group.name = req.body.name
-    }
-    // If a groupAbout is provided in the body of the patch request, then update the group about for the group 'groupID'
-    if (req.body.about != null) {
-        res.group.about = req.body.about
-    }
-    // If a groupPicture is provided in the body of the patch request, then update the profile picture for the group 'groupID'
-    if (req.body.picture != null) {
-        res.group.picture = req.body.picture
-    }
-    try {
-        const updatedGroup = await res.group.save()
-        res.json(updatedGroup) // Successfully updatedGroup, thus return updated group
-    } catch(error) { //Post request had invalid arguments
-        res.status(400).json({ message: error.message })
-    }
+   
 });
 
 // Create new group 
@@ -80,6 +86,27 @@ router.post("/add/:groupID", async (req, res) => {
     // TODO: check userID is valid 
     let userID = req.body.userID; 
     let groupID = req.params.groupID;
+    let user = await User.findById(userID, (err, use) => {
+        if (err) return res.status(500).send({success: false, message: err.toString()});
+    });
+    if (!user) return res.status(404).send({success: false, message: "User with username " + userID + " does not exist"});
+    Group.findById(groupID, (err, group) => {
+        console.log(group);
+        if (err) return res.status(500).send({success: false, message: err.toString()});
+        if (!group) return res.status(404).send({success: false, message: "Group with Id " + groupID + " does not exist"});
+        if (group.members.includes(userID)) return res.status(404).send({success: false, message: "Member is already in group"});
+        group.members.push(userID);
+        Group.findByIdAndUpdate(groupID, {members: group.members}, (err, group) => {
+            if (err) return res.status(500).send({success: false, message: err.toString()});
+            return res.json({success: true});
+        });
+    });
+});
+/*// Add new member to group 
+router.post("/add/:groupID", async (req, res) => {
+    // TODO: check userID is valid 
+    let userID = req.body.userID; 
+    let groupID = req.params.groupID;
     let user = await User.findById(userID, (err, user) => {
         if (err) return res.status(500).send({success: false, message: err.toString()});
     });
@@ -95,24 +122,7 @@ router.post("/add/:groupID", async (req, res) => {
             return res.json({success: true});
         });
     });
-})
-/*router.post("/:groupID", getGroup, async (req, res) => {
-    try{
-        let userID = req.body.userID;
-        // check user exists
-        if (!await userExists(userID)) {
-            res.status(404).json({message: "User does not exist"});
-        }
-
-        res.group.members.push(userID); 
-        res.group.save(); 
-        console.log("Group member added"); 
-
-        res.status(200).send(); 
-    } catch(err) {
-        res.status(400).json({ message: err.message})
-    }
-}); */
+})*/
 
 async function userExists(id) {
     let found = await User.findById(id); 
@@ -125,7 +135,7 @@ async function userExists(id) {
 async function getGroup(req, res, next) {
     let group
     try {
-        group = await Group.findById(req.params.groupID).populate('members', '_id role');
+        group = await Group.findById(req.params.groupID).populate('members', 'about', 'picture', 'name');
     } catch(error) {
        return res.status(404).json({ message: 'Group does not exist'})
     }
@@ -134,6 +144,23 @@ async function getGroup(req, res, next) {
 }
 
 // Remove member from group 
+router.delete("/delete/:groupID/:userID", async (req, res) => {
+    let userID = req.params.userID; 
+    let groupID = req.params.groupID;
+
+    Group.findById(groupID, (err, group) => {
+        if (err) return res.status(500).send({success: false, message: err.toString()});
+        if (!group) return res.status(404).send({success: false, message: "Group with Id " + groupID + " does not exist"});
+        if (!group.members.includes(userID)) return res.status(404).send({success: false, message: "Member not group"});
+        for (i = 0; i < group.members.length; i++) if(group.members[i] == userID) group.members.splice(i, 1);
+        Group.findByIdAndUpdate(groupID, {members: group.members}, (err, groups) => {
+            if (err) return res.status(500).send({success: false, message: err.toString()});
+            return res.json({success: true});
+        });
+    });
+});
+
+/*// Remove member from group 
 router.delete("/delete/:groupID/:userID", async (req, res) => {
     let userID = req.params.userID; 
     let groupID = req.params.groupID;
@@ -150,26 +177,22 @@ router.delete("/delete/:groupID/:userID", async (req, res) => {
             return res.json({success: true});
         });
     });
+});*/
+
+// Delete group 
+router.delete("/delete/:groupID/", async (req, res) => {
+    let groupID = req.params.groupID;
+
+    Group.findById(groupID, async (err, group) => {
+        if (err) return res.status(500).send({success: false, message: err.toString()});
+        if (!group) return res.status(404).send({success: false, message: "Group with Id " + groupID + " does not exist"});
+        Group.findByIdAndDelete(group, (err,  delObj) => {
+            return res.json({success: true})
+        })
+        //Group.deleteById(groupID);
+        
+    });
 });
 
-/*router.delete("/:groupID/:userID", getGroup, async (req, res) => {
-    let userID = req.params.userID;
-    let groupID = req.params.groupID; 
-
-    try {
-        for (let i = 0; i < res.group.members; i++) {
-            if (res.group.members[i].toString() == userID) {
-                res.group.members.splice(i, 1); 
-                res.group.save(); 
-                console.log("Group member " + userID + "removed from " + groupID); 
-                res.status(200).send(); 
-            }
-        }
-    } catch(error) {
-        res.status(500).send({error: error.message}); 
-    }
-    // could not find group member; returns error 
-    res.status(404).json({message: 'Member does not exist or is not in group'}); 
-}); */
 
 module.exports = router; 
--- frontend/src/controller.js
+++ frontend/src/controller.js
@@ -1,26 +1,21 @@
 /*jshint esversion: 10*/
 
 const APIAccess = {
-    updateGroupProfile(gid, groupName, about, picture){
+    updateGroupProfile(group, groupName, about, picture){
         try{
-            return fetch('http://localhost:8000/group/' + gid, {
+            return fetch('http://localhost:8000/group/' + group, {
                 method: 'PATCH',
                 headers: {'Content-Type':'application/json'},
                 body: JSON.stringify({name: groupName, about: about, picture: picture})
             }).then(async (response) => {
                 let jsonRes = await response.json();
                 console.log(jsonRes);
-                if(!jsonRes.success) {  // Display error message based off jsonRes message if register fails
-                    var error = document.getElementById("error-message");
-                    error.style.visibility = "visible";
-                    console.log(jsonRes.message);
-                    error.innerHTML = jsonRes.message + '*';
-                }
+               
                 if(!jsonRes.success) throw jsonRes.message;
                 
                 var error = document.getElementById("error-message");
                 error.style.visibility = "hidden";
-                return {user: jsonRes.name, role: jsonRes.about};
+                return {name: jsonRes.name, about: jsonRes.about};
             });
         }catch(err){
             throw err;
@@ -53,31 +48,6 @@ const APIAccess = {
         }
     },
 
-    /*addGroupMember(gid, _id){
-        try{
-            return fetch('http://localhost:8000/group/' + gid, { // Group id here
-                method: 'POST',
-                headers: {'Content-Type':'application/json'},
-                body: JSON.stringify({userID: _id, groupID: gid}) // member id here
-            }).then(async (response) => {
-                let jsonRes = await response.json();
-                console.log(jsonRes);
-                if(!jsonRes.success) {  // Display error message based off jsonRes message if register fails
-                    var error = document.getElementById("error-message");
-                    error.style.visibility = "visible";
-                    console.log(jsonRes.message);
-                    error.innerHTML = jsonRes.message + '*';
-                }
-                if(!jsonRes.success) throw jsonRes.message;
-                
-                var error = document.getElementById("error-message");
-                error.style.visibility = "hidden";
-                return {user: jsonRes.name, role: jsonRes.about};
-            });
-        }catch(err){
-            throw err;
-        }
-    },*/
     addMember(group, member) {
         try{
             fetch('http://localhost:8000/group/add/' + group, {
@@ -98,31 +68,6 @@ const APIAccess = {
         }
     },
 
-   /* removeMember(gid, _id){
-        try{
-            return fetch('http://localhost:8000/group/' + gid + "/" + _id, { // Group id and member id here
-                method: 'DELETE'
-                //headers: {'Content-Type':'application/json'},
-               // body: JSON.stringify({_id: _id}) // member id here
-            }).then(async (response) => {
-                let jsonRes = await response.json();
-                console.log(jsonRes);
-                if(!jsonRes.success) {  // Display error message based off jsonRes message if register fails
-                    var error = document.getElementById("error-message");
-                    error.style.visibility = "visible";
-                    console.log(jsonRes.message);
-                    error.innerHTML = jsonRes.message + '*';
-                }
-                if(!jsonRes.success) throw jsonRes.message;
-                
-                var error = document.getElementById("error-message");
-                error.style.visibility = "hidden";
-                return {user: jsonRes.name, role: jsonRes.about};
-            });
-        }catch(err){
-            throw err;
-        }
-    }*/
     removeMember(group, member){
         try{
             fetch('http://localhost:8000/group/delete/' + group + '/' + member, {
@@ -137,7 +82,24 @@ const APIAccess = {
         } catch {
 
         }
+    },
+
+    deleteGroup(group){
+        try{
+            fetch('http://localhost:8000/group/delete/' + group, {
+                method: 'DELETE',
+                credentials: 'include',
+            })
+            .then(async (response) => {
+                let jsonRes = await response.json();
+                console.log(jsonRes);
+                return "Success";
+            });
+        } catch {
+
+        }
     }
+
 };
 
 export default APIAccess;
