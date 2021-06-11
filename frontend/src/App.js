535baeeec1d973ae5262c2cfdb2958f1ab1292c7
Chris <christophersuhse@gmail.com>
2021-06-11 09:40:07 -0400

    Added create group, edit group profile.
    Added create group, edit group profile.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Login from './components/pages/Login'
 import Landing from './components/pages/Landing'
 import NavigationBar from './components/NavigationBar'
 import GroupProfile from './components/pages/GroupProfile'
+import GroupProfileEdit from './components/pages/GroupProfileEdit';
+import GroupProfileCreate from './components/pages/GroupProfileCreate';
 
 import {
   BrowserRouter as Router,
@@ -22,6 +24,8 @@ function App() {
         <Route path="/login" component={Login}/>
         <Route path="/test" component={NavigationBar}/>
         <Route path="/register" component={Registration}/>
+        <Route path="/groupProfile/edit" component={GroupProfileEdit}/>
+        <Route path="/groupProfile/create" component={GroupProfileCreate}/>
         <Route path="/groupProfile" component={GroupProfile}/>
       </Switch>
     </Router>
---
+++ frontend/src/components/pages/GroupProfileEdit.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./GroupProfileEdit.css";
+/*jshint esversion: 10*/
+import APIAccess from "../../controller.js";
+
+function GroupProfileEdit() {
+  const update = async (e) => {
+    e.preventDefault();
+    try {
+      let groupName = document.getElementById("groupName").value;
+      let about = document.getElementById("about").value;
+      let picture = document.getElementById("picture").value;
+      console.log(groupName, about, picture);
+      let user = await APIAccess.updateGroupProfile(groupName, about, picture);
+      console.log("Made it here");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="register">
+      <form id="register-form">
+        <fieldset>
+          <header>
+            <h1 className="register-title">Ampact</h1>
+          </header>
+          <main>
+            <h2 className="register-subtitle">Edit Group Information</h2>
+            <div class="wrapper-register">
+              <label for="picture">GroupPicture:</label>
+              <input
+                type="register"
+                name="picture"
+                id="picture"
+                placeholder=""
+              />
+            </div>
+
+            <div class="wrapper-register">
+              <label for="about">About:</label>
+              <div>
+                <input type="register" name="about" id="about" placeholder="" />
+              </div>
+            </div>
+            <div class="wrapper-register">
+              <label for="groupName">GroupName:</label>
+              <div>
+                <input
+                  type="register"
+                  name="groupName"
+                  id="groupName"
+                  placeholder=""
+                />
+              </div>
+            </div>
+            <button type="submit" onClick={update} className="submit-button">Update</button>
+          </main>
+        </fieldset>
+      </form>
+    </div>
+  );
+}
+
+export default GroupProfileEdit;
