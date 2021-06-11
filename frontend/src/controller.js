37cd42eec4a2208d08d1c07d76ca7e36027aa0ee
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-06-10 23:14:40 -0400

    IN-48 corrected merge conflict

--- frontend/src/controller.js
+++ frontend/src/controller.js
@@ -1,6 +1,34 @@
 /*jshint esversion: 10*/
 
 const APIAccess = {
+
+    currentUser(){
+        var username = document.cookie.split("username=")[1];
+        if(!username) return "";
+        if (username.length === 0) return "";
+        return username;
+    },
+
+    //sign in user
+    signInUser(username, password){
+        try{
+            fetch('http://localhost:8000/signin', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {'Content-Type':'application/json'},
+                body: JSON.stringify({username: username, password: password})
+            }).then(async (response) => {
+                let jsonRes = await response.json();
+                if(!jsonRes.success) {
+                    throw jsonRes.message;
+                }
+                window.location = "/";
+            });
+        } catch(err){
+            throw err;
+        }
+    },
+
     registerUser(username, password, role){
         try{
             return fetch('http://localhost:8000/signup', {
@@ -10,15 +38,14 @@ const APIAccess = {
             }).then(async (response) => {
                 let jsonRes = await response.json();
                 console.log(jsonRes);
-                if(!jsonRes.success) {  // Display error message based off jsonRes message if register fails
-                    var error = document.getElementById("error-message");
+                var error = document.getElementById("error-message");
+                if (!jsonRes.success) {  // Display error message based off jsonRes message if register fails
                     error.style.visibility = "visible";
                     console.log(jsonRes.message);
                     error.innerHTML = jsonRes.message + '*';
+                    throw jsonRes.message;
                 }
-                if(!jsonRes.success) throw jsonRes.message;
                 
-                var error = document.getElementById("error-message");
                 error.style.visibility = "hidden";
                 return {user: jsonRes.username, role: jsonRes.role};
             });
