1e75b18dab996c3e32fa0d5c2af570c724444797
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-06-10 21:14:28 -0400

    IN-46 added redirection from registration page

--- frontend/src/components/pages/Login.js
+++ frontend/src/components/pages/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Login.css'
 import Logo from '../../assets/logo.png'
-import Link from "react-router-dom/Link"
+import { Link } from "react-router-dom"
 
 function Login() {
 	return (
