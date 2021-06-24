66f0c9d5174a74f89a9b46d95eda98eea26ff54a
David Tan <45835700+davetanCS@users.noreply.github.com>
2021-06-24 17:50:30 -0400

    [IN-56] Made it so the single video page now grabs the video and their info from the database

--- frontend/src/components/pages/SingleVideoPage.js
+++ frontend/src/components/pages/SingleVideoPage.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SingleVideoPage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Logo from "../../assets/logo.png";
 import APIAccess from "../../controller.js";
 import { useHistory } from "react-router-dom";
 import { AuthService } from "../../util/authService";
+import { useParams } from "react-router-dom";
+
+let mock_data = 
+  [{
+    title: "one",
+    poster: "david",
+    subject: "bootstrap",
+    description: "Lorem ipsum",
+    videoUrl: "https://www.youtube.com/embed/Ih2N_vPI9FY",
+  }
+];
+
+let videoInfo;
 
 function SingleVideoPage() {
+  console.log(mock_data.title);
+  let {vid} = useParams()
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideoData, setLoadedVideoData] = useState([]); 
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("http://localhost:8000/video/?id=" + vid, {
+    })
+    
+      .then((response) => {
+       return response.json()
+      })
+      .then((data) => {
+        mock_data = data;
+        console.log(mock_data);
+        setLoadedVideoData(data);
+        setIsLoading(false);
+       
+     
+      });
+  }, [vid]); 
+
+  {mock_data.map((video) => {
+    videoInfo = video;
+})}
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <div className="container-fluid mb-5">
       <div className="wrapper">
@@ -21,44 +69,20 @@ function SingleVideoPage() {
       <div className="embed-responsive embed-responsive-16by9">
         <iframe
           class="embed-responsive-item"
-          src="https://www.youtube.com/embed/eow125xV5-c"
+          src={videoInfo.videoUrl}
           id="video"
           alllowfullscreen
         ></iframe>
       </div>
       <div className="container-fluid video-info ">
       <div class="d-flex flex-row">
-        <h1 className="mt-2 flex-fill" id="title">Bootstrap Tutorial</h1>
-        <h2 className="mt-2" id="poster">Posted by: David Tan</h2>
+        <h1 className="mt-2 flex-fill" id="title">{videoInfo.title}</h1>
+        <h2 className="mt-2" id="poster">Posted by: {videoInfo.poster}</h2>
         </div>
-        <h3 className="text-secondary" id="subject">Subject: Bootstrap</h3>
+        <h3 className="text-secondary" id="subject">Subject: {videoInfo.subject}</h3>
         <p className="text-start" id="description">
-          Description Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Aenean quis vulputate felis. Integer laoreet volutpat scelerisque.
-          Praesent vel arcu ac magna ultrices consequat in sit amet justo.
-          Vestibulum posuere urna lorem, quis hendrerit ante consequat in.
-          Aliquam erat volutpat. Cras maximus laoreet massa, ut venenatis sapien
-          dapibus quis. Morbi at finibus libero, eget pretium lorem. Aliquam
-          semper efficitur vestibulum. Aliquam erat volutpat. Nulla ornare
-          libero ac porttitor consectetur. In non dui pulvinar enim iaculis
-          sollicitudin eu id nisl. Curabitur placerat risus metus, eget egestas
-          ex tincidunt egestas. Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit. Etiam gravida quis lacus et pharetra. Mauris eleifend
-          metus non fringilla hendrerit. Nulla ultricies ut massa non
-          vestibulum. Praesent vel varius ligula, non pharetra dui. In lacus
-          neque, blandit quis augue et, suscipit consectetur nulla. Phasellus in
-          libero ac ipsum elementum dignissim et id lectus. Donec nec felis
-          maximus, imperdiet turpis non, vehicula libero.
+        {videoInfo.description}
         </p>
-
-        <div class="row align-items-start">
-          <div class="col-md">One of three columns</div>
-          <div class="col-md">One of three columns</div>
-          <div class="col-md">One of three columns</div>
-          <div class="col-md">One of three columns</div>
-          <div class="col-md">One of three columns</div>
-          <div class="col-md">One of three columns</div>
-        </div>
       </div>
       </div>
     </div>
