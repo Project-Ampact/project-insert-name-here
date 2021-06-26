7306646b71a2d2063394b1ae0f9ae61f609aa5ca
Kris <63805048+ravence@users.noreply.github.com>
2021-06-25 23:54:31 -0400

    IN-35 - Implemented endpoint for profile search

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express"); 
 const Group = require("../models/group");
-const User = require("../models/user");
+const Profile = require("../models/profile");
 const router = express.Router();
 
 
@@ -26,5 +26,26 @@ router.get("/group", async (req, res) => {
     return res.json(group);
 });
 
+router.get("/profile", async (req, res) => {
+    let searchString = req.query.searchString; 
+    let page = req.query.page;
+    if (searchString == null) {
+        return res.status(400).send({success: false, message: "Missing required fields"});
+    }
+
+    (page > 0) ? page = parseInt(page) : page = 1;
+
+    let profile;
+    try {
+    profile = await Profile.find(
+        {$text: {
+            $search: searchString,
+            $caseSensitive: false
+        }}).skip((page-1)*15).limit(15);
+    } catch {
+        return res.status(500).send({success: false, message: "Error while searching"});
+    }
+    return res.json(profile);
+});
 
 module.exports = router; 
