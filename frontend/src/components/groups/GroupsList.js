47fa07f4199fbcfc89d6146a91cef7b226746f9a
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-06-29 23:19:32 -0400

    IN-65: refactored create form into its own component

--- frontend/src/components/groups/GroupsList.js
+++ frontend/src/components/groups/GroupsList.js
@@ -4,8 +4,8 @@ import "./GroupsList.css";
 import "../groupProfile/Group.css";
 
 function GroupsList(props) {
-    return (<div>
-        <h1 className="group-members">Groups</h1>
+    return (
+    <div>
         <CardColumns>
             {props.groups.map((group) => {
                 return <Groups key={group._id} name={group.name} role={group._id}/>
