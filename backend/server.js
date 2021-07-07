322e9a5e2cfa93adc7cefe013db6c3476d24fbbd
Kris <63805048+ravence@users.noreply.github.com>
2021-07-06 22:27:28 -0400

    IN-87 - Set up routes

--- backend/server.js
+++ backend/server.js
@@ -149,9 +149,11 @@ mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useFindAndModify: f
 const groups = require('./routes/groupRoutes');
 const videos = require('./routes/videoRoutes');
 const search = require('./routes/searchRoutes');
+const post = require('./routes/postRoutes');
 app.use('/group', groups);
 app.use('/video', videos);
 app.use('/search', search);
+app.use('/post', post);
 
 const profiles = require('./routes/profileRoutes');
 app.use('/profile', profiles);
