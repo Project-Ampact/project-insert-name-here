be3f708387791aa8e8133b6dff30fa2e5e170041
Chris <christophersuhse@gmail.com>
2021-07-08 16:58:19 -0400

    Completed delete comment endpoint
    Completed delete comment endpoint

--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 const router = express.Router();
 
 // General note: Both replies and comments are one in the same. The difference lies in relative perspective;
-//               a commment made on a comment is considered a reply, however that reply is also a comment which could have replies.
+//               a commment made on a comment is considered a reply, however that reply is also a comment.
 
 // Get replies given commentId
 router.get("/:commentID", async (req, res) => {
@@ -29,9 +29,11 @@ router.get("/:commentID", async (req, res) => {
   console.log(specifiedComment.replies);
 
   var allReplies = [];
-  // Given an array of reply id's, get the reply object matching its id and push into the allReplies array 
+  // Given an array of reply id's, get the reply object matching its id and push into the allReplies array
   for (let i = 0; i < specifiedComment.replies.length; i++) {
-    const curReply = await Comment.findById(specifiedComment.replies[i], (err, replies) => {
+    const curReply = await Comment.findById(
+      specifiedComment.replies[i],
+      (err, replies) => {
         if (err)
           return res
             .status(500)
@@ -41,44 +43,18 @@ router.get("/:commentID", async (req, res) => {
             .status(404)
             .send({ success: false, message: "Reply not found" });
         return replies;
-      });
+      }
+    );
 
-      allReplies.push(curReply);
+    allReplies.push(curReply);
   }
-  
+
   return res.json(allReplies);
 });
 
 //Get all comments given a post id
 
-/*// Update a group attribute(s) 
-router.patch("/:groupID", async (req, res) => {
-    let groupId = req.params.groupID;
-    let groupP
-    Group.findById(groupId, async (err, groups) => {
-        if (req.body.name != null) {
-            groups.name = req.body.name
-        }
-        // If a groupAbout is provided in the body of the patch request, then update the group about for the group 'groupID'
-        if (req.body.about != null) {
-            groups.about = req.body.about
-        }
-        // If a groupPicture is provided in the body of the patch request, then update the profile picture for the group 'groupID'
-        if (req.body.picture != null) {
-            groups.picture = req.body.picture
-        }
-        try {
-            const updatedGroup = await groups.save()
-            res.json(updatedGroup) // Successfully updatedGroup, thus return updated group
-        } catch(error) { //Post request had invalid arguments
-            res.status(400).json({ message: error.message })
-        }
-    });
-    // If a groupName is provided in the body of the patch request, then update the group name for the group 'groupID'
-   
-});*/
-
-// Create new comment
+// Create new comment TODO: Need to ensure commentids are added to post 'postId'
 router.post("/", async (req, res) => {
   let message = req.body.message;
   let poster = req.body.poster;
@@ -86,7 +62,7 @@ router.post("/", async (req, res) => {
 
   try {
     const savedComment = await comment.save();
-    console.log("Group created");
+    // console.log("Group created");
     res.status(201).json(savedComment); // Successfully created comment, thus return new comment info as json
   } catch (err) {
     // Error in creating a new comment, thus return error message (bad data)
@@ -135,7 +111,7 @@ router.post("/add/:commentID", async (req, res) => {
   });
 });
 
-// Delete comment Note that this needs more work to make it delete all of its replies
+// Delete comment TODO: needs more work to make it delete all of its replies
 router.delete("/delete/:commentID/", async (req, res) => {
   let commentID = req.params.commentID;
 
@@ -147,6 +123,12 @@ router.delete("/delete/:commentID/", async (req, res) => {
         success: false,
         message: "Comment with Id " + commentID + " does not exist",
       });
+
+    //Delete all the replies of a comment Note: Does not delete replies of replies
+    for (let i = 0; i < comment.replies.length; i++) {
+      Comment.findByIdAndDelete(comment.replies[i], (err, delObj) => {});
+    }
+    //Delete the comment itself
     Comment.findByIdAndDelete(comment, (err, delObj) => {
       return res.json({ success: true });
     });
