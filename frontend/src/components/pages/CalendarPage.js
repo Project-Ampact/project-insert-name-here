725c0beb8c429dc553f50be343d63dccce8cff09
ma-ray <60418739+rayma01@users.noreply.github.com>
2021-07-08 23:54:47 -0400

    IN-99: added event color types

--- frontend/src/components/pages/CalendarPage.js
+++ frontend/src/components/pages/CalendarPage.js
@@ -7,18 +7,44 @@ import './CalendarPage.css'
 
 const mock_data = [
   {
-    title: 'Sprint Meeting',
+    title: 'Personal Meeting',
+    description: 'Test desription',
     start: new Date('July 10, 2021 03:00:00'),
-    end: new Date('July 10, 2021 05:00:00')
+    end: new Date('July 10, 2021 05:00:00'),
+    type: 'personal',
+    groupId: 'testgroup',
+    userId: 'testuser',
   },
   {
-    title: 'Sprint Meeting',
-    start: new Date('July 12, 2021 12:00:00'),
-    end: new Date('July 12, 2021 13:00:00')
+    title: 'Group Meeting',
+    description: 'Test desription',
+    start: new Date('July 15, 2021 03:00:00'),
+    end: new Date('July 15, 2021 05:00:00'),
+    type: 'group',
+    groupId: 'testgroup',
+    userId: 'testuser'
+  },
+  {
+    title: 'General Meeting',
+    description: 'Test desription',
+    start: new Date('July 17, 2021 03:00:00'),
+    end: new Date('July 17, 2021 05:00:00'),
+    type: 'general',
+    groupId: 'testgroup',
+    userId: 'testuser'
   }
 ]
 
 function CalendarPage() {
+
+  const convertData = data => {
+    let eventColor;
+    if (data.type === 'personal') eventColor = '#54e0ff'
+    else if (data.type === 'group') eventColor = '#80eb34'
+    else if (data.type === 'general') eventColor = '#ff5454'
+    return {...data, color: eventColor}
+  }
+
   return (
     <PageLayout>
       <div className="container-xl mt-5 card calendar">
@@ -30,7 +56,8 @@ function CalendarPage() {
           customButtons={{addEvent: {text: "Add Event"}}} // add functionality for button here
           headerToolbar={{left: "addEvent", center: "title", right: "today dayGridMonth,timeGridWeek,timeGridDay prev,next"}}
           buttonText={{today: 'Today', month: 'Month', week: 'Week', day: 'Day'}}
-          events={mock_data}
+          events={mock_data.map(x => convertData(x))}
+          dayMaxEvents={true}
           />
       </div>
     </PageLayout>
