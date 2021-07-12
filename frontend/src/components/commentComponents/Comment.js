78ab82cef84cf493277f328b1aba7a9226d68e69
Chris <christophersuhse@gmail.com>
2021-07-12 17:58:46 -0400

    Created frontend components for commentSection
    Created frontend components for commentSection, comment, replies, replySection.

---
+++ frontend/src/components/commentComponents/Comment.js
@@ -0,0 +1,26 @@
+import "./Comment.css";
+import React, { useState, useEffect, useContext } from "react";
+import { Row, Container, Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReplySection from "./ReplySection";
+
+function Comment(props) {
+  return (
+    <Container>
+      <Row className="cus2-row">
+        <Card className="comment-wrapper rounded">
+          <Card.Body className="comment-body">
+            <div className="title-and-date">
+              <h3 className="card-title2">{props.user}</h3>
+              <h6 className="text-date comment-date">{props.date}</h6>
+            </div>
+            <p className="text-content">{props.content}</p>
+          </Card.Body>
+          <ReplySection></ReplySection>
+        </Card>
+      </Row>
+    </Container>
+  );
+}
+
+export default Comment;
---
+++ frontend/src/components/commentComponents/ReplySection.js
@@ -0,0 +1,95 @@
+import "./CommentSection.css";
+import React, { useState, useEffect, useContext } from "react";
+import {
+  Row,
+  Container,
+  Accordion,
+  AccordionCollapse,
+  AccordionContext,
+  AccordionToggle,
+  useAccordionToggle,
+  Card,
+  Button,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Reply from "./Reply";
+
+let mock_data = [
+  {
+    user: "David Tan",
+    type: "general",
+    content: "Lorem ipsum content is here cool",
+    date: "today lol",
+  },
+  {
+    user: "Christopher Suh",
+    type: "general",
+    content: "Lorem ipsum content is here cool2",
+    date: "today lol2",
+  },
+];
+
+function Expand({ children, eventKey, callback }) {
+  const currentEventKey = useContext(AccordionContext);
+
+  const decoratedOnClick = useAccordionToggle(
+    eventKey,
+    () => callback && callback(eventKey)
+  );
+
+  const isCurrentEventKey = currentEventKey === eventKey;
+
+  return (
+    <Button
+      type="button"
+      //  style={{ backgroundColor: isCurrentEventKey ? "blue" : "grey" }}
+      onClick={decoratedOnClick}
+    >
+      {children}
+    </Button>
+  );
+}
+
+function LoadReplies() {
+  return mock_data.map((mock_data_piece) => {
+    let date = new Date(mock_data_piece.date);
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (day < 10) {
+      day = "0" + day;
+    }
+
+    console.log(date.getFullYear() + "/" + month + "/" + day);
+    return (
+      <Reply 
+        className="loaded-comment"
+        user={mock_data_piece.user}
+        type={mock_data_piece.type}
+        date={date.getFullYear() + "/" + month + "/" + day}
+        content={mock_data_piece.content}
+      />
+    );
+  });
+}
+
+function ReplySection(props) {
+  return (
+    <Accordion>
+      <Card>
+        <Card.Header>
+          <Expand eventKey="0">Replies</Expand>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Container className="loaded-comments">
+              {LoadReplies()}
+          </Container>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+}
+
+export default ReplySection;
