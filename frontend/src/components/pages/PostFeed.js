8e9b131233f11488c8ceedfa5d083f1c2bd5c6e1
David Tan <45835700+davetanCS@users.noreply.github.com>
2021-07-12 20:46:01 -0400

    [IN-84] Added create posts to both announcements and discussion. Creates post based on the type, current user and their description

--- frontend/src/components/pages/PostFeed.js
+++ frontend/src/components/pages/PostFeed.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row, Container } from "react-bootstrap";
+import { Row, Container, Form, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { useParams } from "react-router-dom";
@@ -10,21 +10,19 @@ import PageLayout from "./DefaultPage";
 import "./PostFeed.css";
 import "react-pro-sidebar/dist/css/styles.css";
 import Post from "../postComponents/Post.js";
-
+import { AuthService } from "../../util/authService";
 let mock_data = [
   {
     user: "David Tan",
-    type: "general",
+    type: "QnA",
     content: "Lorem ipsum content is here cool",
     date: "today lol",
   },
 ];
 
-// let postInfo = mock_data;
-
-
-
 function PostFeed() {
+  let auth = AuthService();
+  const username = document.cookie.split("user=")[1].split("%20")[0];
   const [isLoading, setIsLoading] = useState(true);
   const [loadedUserData, setLoadedUserData] = useState([]);
   useEffect(() => {
@@ -38,6 +36,17 @@ function PostFeed() {
       });
   });
 
+  useEffect(() => {
+    async function fetchData() {
+      let data = await APIAccess.getUserProfile(username);
+      return data;
+    }
+    fetchData().then((x) => {
+      setLoadedUserData(x._id);
+      console.log("USER!: " + x._id);
+    });
+  }, [username]);
+
   if (isLoading) {
     return (
       <section>
@@ -46,11 +55,53 @@ function PostFeed() {
     );
   }
 
-
+  const update = async (e) => {
+    e.preventDefault();
+    try {
+      let postDescription = document.getElementById("postCreate").value;
+      let type = "QnA";
+      let user;
+      console.log(postDescription);
+      user = loadedUserData;
+      console.log("USER" + user);
+      window.location.reload();
+      await APIAccess.createPost(user, type, postDescription);
+      console.log("Made it here");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <PageLayout>
       <div id="posts">
+        <Container>
+          <div class="card" id="post-wrapper">
+            <div class="card-body rounded" id="post-body">
+              <div class="flex-container">
+                <h1 class="card-title">Create Post As: {username}</h1>
+              </div>
+              <Form className="form-cus">
+                <Form.Group controlId="postCreate">
+                  <Form.Control
+                    as="textarea"
+                    rows={4}
+                    type="postCreate"
+                    placeholder="Type your post here!"
+                  />
+                </Form.Group>
+                <Button
+                  onClick={update}
+                  id="submit-button"
+                  variant="primary"
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </Container>
         {mock_data.map((mock_data_piece) => {
           let date = new Date(mock_data_piece.date);
           let month = date.getMonth() + 1;
@@ -61,12 +112,12 @@ function PostFeed() {
           if (day < 10) {
             day = "0" + day;
           }
-          
+
           console.log(date.getFullYear() + "/" + month + "/" + day);
-          if (mock_data_piece.type == "general") {
+          if (mock_data_piece.type == "QnA") {
             return (
               <Post
-                user={mock_data_piece.user}  
+                user={mock_data_piece.user}
                 type={mock_data_piece.type}
                 date={date.getFullYear() + "/" + month + "/" + day}
                 content={mock_data_piece.content}
