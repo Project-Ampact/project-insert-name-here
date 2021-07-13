ef746b54c0fb840f8c297dcfac82e238283ae26e
Hui Z <46905323+selinaz1@users.noreply.github.com>
2021-07-13 17:07:45 -0400

    IN-7: Dlbs list page
    -DlbsSubmit.js : adding the description field
    -Dlbs.js is the page samples (ideal appeal)
    -not sure how to connect to the back end -> DlbsItem.js / DlbsList.js

--- frontend/src/components/dlbsComponents/DlbsSubmit.js
+++ frontend/src/components/dlbsComponents/DlbsSubmit.js
@@ -7,49 +7,7 @@ import PageLayout from "../pages/DefaultPage";
 import { Col, Row, Form, Button, Container } from "react-bootstrap";
 
 function DlbsSubmit() {
-  let { gid } = useParams();
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadedGroupData, setLoadedGroupData] = useState([]);
-  
-  useEffect(() => {
-    //setIsLoading(true);
-    fetch("http://localhost:8000/group/" + gid)
-      .then((response) => {
-        // console.log( response.json())
-        return response.json();
-        //  setLoadedGroupData(response.json());
-        //  setIsLoading(false)
-      })
-      .then((data) => {
-        setLoadedGroupData(data);
-        setIsLoading(false);
-      });
-  }, [gid]);
-
-  const update = async (e) => {
-    try {
-      let about = document.getElementById("about").value;
-
-      //console.log("Made it here");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const deleteGroup = async (e) => {
-    e.preventDefault();
-    try {
-      await APIAccess.deleteGroup(gid);
-      console.log("Made it here");
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-
-  let createGroups = "http://localhost:3000/groupProfile/create";
-  let goToGroupProfile = "http://localhost:3000/groupProfile/" + gid;
+  let backtoDlbs = "http://localhost:3000/Dlbs";  
 
   return (
     <PageLayout>
@@ -68,11 +26,19 @@ function DlbsSubmit() {
                     <Form.Text className="text-muted">
                       This is the name of your assignment you are currently submiting.
                     </Form.Text>
-                    <Button onClick={update} href={goToGroupProfile} variant="primary" type="submit">
+                    <Button href={backtoDlbs} variant="primary" type="submit">
                     change
                   </Button>
                   </Form.Group>
-                  <Form.Group controlId="dlbsddl">
+
+                  <Form.Group controlId="details">
+                    <Form.Control type="details" as="textarea" 
+                    placeholder="This is the first sample assignment. You will respected to ...
+                    etc..."
+                    rows={2} plaintext readOnly />
+                  </Form.Group>
+
+                  <Form.Group className="deadline" controlId="dlbsddl">
                     <Form.Label>Assignment Deadline: </Form.Label>
                     <Form.Control
                       type="deadline"
@@ -83,7 +49,7 @@ function DlbsSubmit() {
                     <Form.File id="inputfile" label="Please upload your own work." />
                   </Form.Group>
                   <Form.Group controlId="about">
-                    <Form.Label>Submission Description</Form.Label>
+                    <Form.Label>Submission Note:</Form.Label>
                     <Form.Control type="about" as="textarea" rows={3} />
                   </Form.Group>
 
@@ -91,10 +57,10 @@ function DlbsSubmit() {
                 <div className="register del-button">
                   <Button
                     type="submit"
-                    onClick={update}
+                    //onClick={}
                     className="submitbutton"
                     variant="primary"
-                    href={goToGroupProfile}
+                    href={backtoDlbs}
                     >
                     Submit
                   </Button>
