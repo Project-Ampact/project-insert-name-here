3f6c69484b486c0335e8349065b4e98d2fabf53f
lawrencecai49 <58226504+lawrencecai49@users.noreply.github.com>
2021-07-24 11:03:21 -0400

    IN-105: Fixed bug in backend

--- backend/routes/assignmentRoutes.js
+++ backend/routes/assignmentRoutes.js
@@ -6,7 +6,8 @@ const Submission = require("../models/submission");
 const Authentication = require("../authentication");
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const path = require("path");
+const upload = multer({ dest: path.join(__dirname, '..', 'uploads')});
 
 // Add assignment
 router.post("/", Authentication.isAuthenticated, Authentication.isInstructor, async(req, res, next) => {
@@ -31,21 +32,27 @@ router.post("/", Authentication.isAuthenticated, Authentication.isInstructor, as
 });
 
 // Add submission for a given assignment
-router.post("/submission", Authentication.isAuthenticated, upload.single('file') , async(req, res) => {
-    let assignment = req.body.assignment;
-    console.log(assignment);
-    let newAssignment = new Submission({
-        user: req.user._id,
-        file: req.file,
-        assignment: assignment
+router.post("/submission", Authentication.isAuthenticated, upload.single('file') , (req, res) => {
+    let assignmentId = req.body.assignment;
+    Deliverable.findById(assignmentId, async(err, assignment) => {
+        if (err) return res.status(500).send({success: false, message: err.toString()});
+        if (!assignment) return res.status(404).send({success: false, message: "Can not find assignment"});
+        let fileEnding = req.file.originalname.split(".").pop();
+        if (!assignment.fileTypes.includes(fileEnding)) return res.status(404).send({success: false, message: "Invalid file type"});
+        let newAssignment = new Submission({
+            user: req.user._id,
+            file: req.file,
+            assignment: assignmentId
+        });
+        try{
+            let savedAssignment = await newAssignment.save();
+            return res.json({success: true, submissionId: savedAssignment._id});
+        }
+        catch(error){
+            return res.status(500).send({success: false, message: error.toString()});
+        }
     });
-    try{
-        let savedAssignment = await newAssignment.save();
-        return res.json({success: true, submissionId: savedAssignment._id});
-    }
-    catch(err){
-        return res.status(500).send({success: false, message: err.toString()});
-    }
+    
 });
 
 //Get assignments
@@ -59,7 +66,7 @@ router.get("/", Authentication.isAuthenticated, async(req, res) => {
     });
 });
 
-//Get Submissions
+//Get metadata for submissions
 router.get("/submission/metadata", Authentication.isAuthenticated, async(req, res) => {
     let query = {};
     if (!req.query.assignment) return res.status(401).send({success: false, message: "Can only get submissions for one assignment"});
@@ -71,14 +78,12 @@ router.get("/submission/metadata", Authentication.isAuthenticated, async(req, re
     });
 });
 
-router.get("/submission/file", Authentication.isAuthenticated, async(req, res) => {
-    let query = {};
-    if (!req.query.assignment) return res.status(401).send({success: false, message: "Can only get submissions for one assignment"});
-    query.assignment = req.query.assignment;
-    if (req.query.id) query._id = req.query.id;
-    Submission.find(query, (err, submissions) => {
+//Get file by id
+router.get("/submission/file/:id", Authentication.isAuthenticated, async(req, res) => {
+    if (!req.params.id) return res.status(401).send({success: false, message: "Request must contain id parameter"});
+    Submission.findById(req.params.id, (err, submission) => {
         if (err) return res.status(500).send({success: false, message: err.toString()});
-        return res.json(submissions);
+        return res.sendFile(path.join(__dirname, ".." , submission.file.path));
     });
 });
 
