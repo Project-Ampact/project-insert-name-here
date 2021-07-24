4864a6baf025bd419507e89902840672e96233a0
lawrencecai49 <58226504+lawrencecai49@users.noreply.github.com>
2021-07-24 11:03:21 -0400

    IN-105: Fixed bug in backend

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(express.static('static'));
+
 //prints request out onto the console
 app.use((req, res, next) => {
     console.log("HTTP request", req.method, req.url, req.body);
@@ -139,6 +141,7 @@ mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useFindAndModify: f
 
 const events = require('./routes/eventRoutes');
 const groups = require('./routes/groupRoutes');
+const assignment = require('./routes/assignmentRoutes');
 const videos = require('./routes/videoRoutes');
 const search = require('./routes/searchRoutes');
 const profiles = require('./routes/profileRoutes');
@@ -146,6 +149,7 @@ const comment = require('./routes/commentRoutes');
 const post = require('./routes/postRoutes');
 
 app.use('/group', groups);
+app.use('/assignment', assignment);
 app.use('/video', videos);
 app.use('/search', search);
 app.use('/post', post);
