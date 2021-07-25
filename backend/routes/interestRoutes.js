2e7b92fe05f6655e45b1ee75ca0765b250882e4d
Kris <63805048+ravence@users.noreply.github.com>
2021-07-24 23:57:34 -0400

    IN-119 - Added endpoint to set interests

--- backend/routes/interestRoutes.js
+++ backend/routes/interestRoutes.js
@@ -14,4 +14,38 @@ router.get("/tags", async(req, res) => {
     }
 });
 
+router.put("/user/:userID", async(req, res) => {
+    let userID = req.params.userID;
+    let interests = req.body.interests;
+
+    if (!interests) return res.status(400).json.send({
+        success: false,
+        message: "Request body must contain interests"
+    });
+
+    if (!await User.exists({_id: userID})) return res.status(404).send({
+        success: false,
+        message: "Could not find user"
+    });
+
+    let updatedInterests; 
+    if (!await Interest.exists({_id: userID})) 
+        updatedInterests = new Interest({_id: userID, interests: interests});
+    else 
+        updatedInterests = await Interest.findById(userID);
+
+    updatedInterests.interests = interests; 
+    
+    try {
+        await updatedInterests.save();
+    } catch (e) {
+        return res.status(500).send({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+
+    return res.status(200).send();
+});
+
 module.exports = router; 
